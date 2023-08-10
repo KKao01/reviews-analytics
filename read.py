369e4a32397664ae95b4ee124cdf81850569af73
KKao01 <u10010204@gmail.com>
2023-08-10 10:22:08 +0800

    03

--- read.py
+++ read.py
@@ -12,3 +12,10 @@ sum_list = 0
 for d in data:
 	sum_list += len(d)
 print('平均字數是: ', sum_list / len(data))
+
+
+new_data = []
+for d in data:
+	if len(d) < 100:
+		new_data.append(d)
+print('總共有', len(new_data), '筆長度小於100')
